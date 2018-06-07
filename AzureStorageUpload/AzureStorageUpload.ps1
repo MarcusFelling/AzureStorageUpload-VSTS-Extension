9e98446875f3379560c14a12cc8937aacde3fce6
rdlucas2 <rdlucas2@gmail.com>
2018-06-07 11:35:07 -0400

    adding a way to include azcopy options

--- AzureStorageUpload/AzureStorageUpload.ps1
+++ AzureStorageUpload/AzureStorageUpload.ps1
@@ -1,11 +1,11 @@
 [CmdletBinding()]
 param()
 
-Function FileUpload($Source, $Pattern, $Dest, $DestKey) {    
+Function FileUpload($Source, $Pattern, $Dest, $DestKey, $AdditionalOptions) {    
 
     # Begin Upload
     Write-Output "Uploading $source/$pattern"
-    & "$PSScriptRoot\AzCopy\AzCopy.exe" /Source:$source /Dest:$Dest /DestKey:$DestKey /Pattern:$Pattern
+    & "$PSScriptRoot\AzCopy\AzCopy.exe" /Source:$source /Dest:$Dest /DestKey:$DestKey /Pattern:$Pattern $AdditionalOptions
 }
 
 Trace-VstsEnteringInvocation $MyInvocation
@@ -16,9 +16,10 @@ Try {
     $Pattern = Get-VstsInput -Name Pattern -Require
     $Dest = Get-VstsInput -Name Dest
     $DestKey = Get-VstsInput -Name DestKey    
+    $AdditionalOptions = Get-VstsInput -Name AdditionalOptions
 
     write-output "Running file Upload"
-    FileUpload $Source $Pattern $Dest $DestKey
+    FileUpload $Source $Pattern $Dest $DestKey $AdditionalOptions
 }
 Catch{
     Write-Error $_.Exception.ToString()
